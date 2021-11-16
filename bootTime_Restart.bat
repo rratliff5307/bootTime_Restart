c4fe24df4aaeb34311d1e54cc68d7d2f9e3f0e68
Ricky <rratliff5307@gmail.com>
2021-11-15 18:04:54 -0600

    Update bootTime_Restart.bat

--- bootTime_Restart.bat
+++ bootTime_Restart.bat
@@ -2,4 +2,4 @@
 set /p pcName="Please enter the name of the computer: "
 SystemInfo /s %pcName% | find "Boot Time:"
 set /p restartPrompt="Enter y to restart %pcname%:"
-if %restartPrompt%==y (shutdown /r /f \\%pcName%) else (echo "have a nice day")
+if %restartPrompt%==y (shutdown /r /f \\%pcName%) else (pause)
